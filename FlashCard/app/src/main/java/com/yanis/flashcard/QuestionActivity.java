704a35d0395acd4c6aa798cb524208ef65528fba
Yanis Verbeke <yanis88357@gmail.com>
2020-03-20 14:01:49 +0100

    Score

--- FlashCard/app/src/main/java/com/yanis/flashcard/QuestionActivity.java
+++ FlashCard/app/src/main/java/com/yanis/flashcard/QuestionActivity.java
@@ -2,6 +2,7 @@ package com.yanis.flashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -22,7 +24,8 @@ public class QuestionActivity extends AppCompatActivity {
 
     boolean isAnswering = true;
     int actualQuestion = 0;
-    int totalQuestionNumber = 2;
+    int totalQuestionNumber = 5;
+    int score = 0;
     List<Question> questionList;
     Question question;
     MediaPlayer mediaPlayer;
@@ -32,22 +35,27 @@ public class QuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
-
         questionList = CreateQuestions();
+        Collections.shuffle(questionList);
         ResetQuestion();
 
         final Button validateButton = findViewById(R.id.validateButton);
         validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextView correctAnswerTextView = findViewById(R.id.correctAnswerTextView);
+                TextView labelAnswerTextView = findViewById(R.id.labelAnswerTextView);
+                RadioGroup questionsRadioGroup = findViewById(R.id.questionsRadioGroup);
                 if (isAnswering) {
-                    TextView correctAnswerTextView = findViewById(R.id.correctAnswerTextView);
-                    TextView labelAnswerTextView = findViewById(R.id.labelAnswerTextView);
-                    RadioGroup questionsRadioGroup = findViewById(R.id.questionsRadioGroup);
                     RadioButton checkedAnswer = findViewById(questionsRadioGroup.getCheckedRadioButtonId());
+                    if (checkedAnswer == null) {
+                        Toast.makeText(QuestionActivity.this, "Entrez une réponse", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                     if (((String) checkedAnswer.getText()).equals(question.getCorrectAnswer())) {
                         correctAnswerTextView.setText("Bonne réponse !");
                         labelAnswerTextView.setText("");
+                        score++;
                     } else {
                         correctAnswerTextView.setText("Mauvaise réponse !");
                         labelAnswerTextView.setText("La bonne réponse était \"" + question.getCorrectAnswer() + "\"");
@@ -60,12 +68,17 @@ public class QuestionActivity extends AppCompatActivity {
                     isAnswering = false;
                 } else {
                     mediaPlayer.stop();
+                    correctAnswerTextView.setText("");
+                    labelAnswerTextView.setText("");
                     if (actualQuestion < totalQuestionNumber - 1) {
                         actualQuestion++;
                         ResetQuestion();
                         validateButton.setText("Valider la réponse");
                     } else {
-
+                        Intent intent = new Intent(QuestionActivity.this, ResultActivity.class);
+                        intent.putExtra("score", score);
+                        intent.putExtra("totalQuestionNumber", totalQuestionNumber);
+                        startActivity(intent);
                     }
                 }
             }
@@ -88,9 +101,18 @@ public class QuestionActivity extends AppCompatActivity {
 
     private List<Question> CreateQuestions() {
         List<Question> questionList = new ArrayList<>();
-
-        questionList.add(new Question(R.raw.map_of_the_problematique, Arrays.asList("Uprising", "Map Of The Problematique", "Bliss", "Psycho"), "Map Of The Problematique"));
-        questionList.add(new Question(R.raw.cant_stop, Arrays.asList("Can't Stop", "Californication", "Give It Away", "Under The Bridge"), "Can't Stop"));
+        questionList.add(new Question(R.raw.map_of_the_problematique, Arrays.asList("Supermassive Black Hole", "Map Of The Problematique", "Bliss", "Starlight"), "Map Of The Problematique"));
+        questionList.add(new Question(R.raw.cant_stop, Arrays.asList("Can't Stop", "By The Way", "Give It Away", "Otherside"), "Can't Stop"));
+        questionList.add(new Question(R.raw.all_along_the_watchtower, Arrays.asList("Hey Joe", "Voodoo Child", "All Along The Watchtower", "Foxey Lady"), "All Along The Watchtower"));
+        questionList.add(new Question(R.raw.ban_all_the_music, Arrays.asList("Wake Up Call", "Trip Switch", "Six Billion", "Ban All The Music"), "Ban All The Music"));
+        questionList.add(new Question(R.raw.brianstorm, Arrays.asList("Fluorescent Adolescent", "Brianstorm", "505", "Balaclava"), "Brianstorm"));
+        questionList.add(new Question(R.raw.by_the_way, Arrays.asList("Can't Stop", "By The Way", "Give It Away", "Otherside"), "By The Way"));
+        questionList.add(new Question(R.raw.citizen_erased, Arrays.asList("Hysteria", "Sunburn", "Citizen Erased", "New Born"), "Citizen Erased"));
+        questionList.add(new Question(R.raw.come_on_over, Arrays.asList("Come On Over", "Out Of The Black", "Little Monster", "Ten Tone Skeleton"), "Come On Over"));
+        questionList.add(new Question(R.raw.immigrant_song, Arrays.asList("Immigrant Song", "Ramble On", "Stairway To Heaven", "Heartbreaker"), "Immigrant Song"));
+        questionList.add(new Question(R.raw.live_like_animals, Arrays.asList("Amsterdam", "Particles", "Sorry", "Live Like Animals"), "Live Like Animals"));
+        questionList.add(new Question(R.raw.r_u_mine, Arrays.asList("R U Mine", "Do I Wanna Know?", "Mad Sounds", "Arabella"), "R U Mine"));
+        questionList.add(new Question(R.raw.ramble_on, Arrays.asList("Ramble On", "Custard Pie", "Kashmir", "Communication Breakdown"), "Ramble On"));
         return questionList;
     }
 
--- FlashCard/app/src/main/java/com/yanis/flashcard/ResultActivity.java
+++ FlashCard/app/src/main/java/com/yanis/flashcard/ResultActivity.java
@@ -2,13 +2,36 @@ package com.yanis.flashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    int score;
+    int totalQuestionNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent scoreIntent = getIntent();
+        score = scoreIntent.getIntExtra("score", 0);
+        totalQuestionNumber = scoreIntent.getIntExtra("totalQuestionNumber", 0);
+
+        TextView scoreTextView = findViewById(R.id.scoreTextView);
+        scoreTextView.setText(score + " / " + totalQuestionNumber);
+
+        Button backMenuButton = findViewById(R.id.backMenuButton);
+        backMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
